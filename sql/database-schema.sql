fe716f8ed93e522b3d8e40dcf54901f1a927ef85
Andra Simionov <andra.simionov@payu.ro>
2017-03-29 21:27:21 +0300

    Add a queries to create tables for sample_bank database

--- sql/database-schema.sql
+++ sql/database-schema.sql
@@ -2,3 +2,28 @@
 CREATE DATABASE IF NOT EXISTS `sample_bank`;
 
 USE `sample_bank`;
+
+CREATE TABLE IF NOT EXISTS `users` (
+	`IdUser` INT(11) NOT NULL AUTO_INCREMENT,
+	`Username` VARCHAR(100) NOT NULL DEFAULT '',
+	`FirstName` VARCHAR(50) NOT NULL DEFAULT '',
+	`LastName` VARCHAR(50) NOT NULL DEFAULT '',
+	`Email` VARCHAR(100) NOT NULL DEFAULT '',
+	`Password` VARCHAR(40) NOT NULL DEFAULT '' COMMENT 'SHA-1 algorithm.',
+	PRIMARY KEY (`IdUser`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+AUTO_INCREMENT=0;
+
+CREATE TABLE IF NOT EXISTS `credit_cards` (
+	`IdCreditCard` INT(11) NOT NULL AUTO_INCREMENT,
+	`IdUser` INT(11) NOT NULL,
+	`CardNumber` INT(19) NOT NULL,
+	`Cvv` INT(4) NOT NULL,
+	`Sold` INT(11) NOT NULL,
+	`ExpirationDate` DATETIME NOT NULL DEFAULT ''
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+AUTO_INCREMENT=0;
