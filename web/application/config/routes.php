deda66d9b0953171409321440af7b90f53c259b3
Andra Simionov <andra.simionov@payu.ro>
2017-04-04 09:32:45 +0300

    Add refund route

--- web/application/config/routes.php
+++ web/application/config/routes.php
@@ -53,5 +53,6 @@ $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 $route['(api/v1/pay)'] = 'SoldController/pay';
+$route['(api/v1/refund)'] = 'SoldController/refund';
 $route['(api/v1/getSold)'] = 'SoldController/getSold';
 $route['(api/v1/getCardData)'] = 'SoldController/getCardData';
